71b8b8dc0d8ef88f121c8a28030f44588483ecbe
jonhowell <j.a.howell@mmu.ac.uk>
2014-09-02 14:49:05 +0100

    Initial Commit

---
+++ Corsi/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Corsi
+//
+//  Created by Jon Howell on 02/09/2014.
+//  Copyright (c) 2014 Manchester Metropolitan University - ESS - essmobile. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
