acc38e3778c752f48c71cb8df9dfaba8d4add54f
jonhowell <j.a.howell@mmu.ac.uk>
2014-09-24 16:48:01 +0100

    added more plist entries

--- Corsi/settingsVC.m
+++ Corsi/settingsVC.m
@@ -64,8 +64,6 @@
 // getting an Float
 //float myFloat = [prefs floatForKey:@"floatKey"];
 
-
-
 @implementation settingsVC
 {
     int start;
@@ -178,64 +176,6 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    mySingleton *singleton = [mySingleton sharedSingleton];
-    
-    NSURL *defaultPrefsFile = [[NSBundle mainBundle]URLForResource:@"Root" withExtension:@"plist"];
-    NSDictionary *defaultPrefs = [NSDictionary dictionaryWithContentsOfURL:defaultPrefsFile];
-    [[NSUserDefaults standardUserDefaults] registerDefaults:defaultPrefs];
-    
-    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-    [[NSUserDefaults standardUserDefaults] synchronize];
-    
-    //read the user defaults in the iPhone/iPad bundle
-    
-    currentBlockColour      = [defaults objectForKey:kBlockCol];
-    currentShowColour       = [defaults objectForKey:kShowCol];
-    currentBackgroundColour = [defaults objectForKey:kBackCol];
-   //[defaults objectForKey:kTester];
-   //[defaults objectForKey:kSubject];
-   //[defaults objectForKey:kEmail];
-   //[defaults objectForKey:kStart];
-   //[defaults objectForKey:kFinish];
-  // [defaults objectForKey:kSize];
-   //[defaults objectForKey:kForward];
-  // [defaults objectForKey:kInfo];
-  // [defaults objectForKey:kRot];
-
-  // [defaults objectForKey:kDelay];
-  // [defaults objectForKey:kTime];
-  // [defaults objectForKey:kShow];
-  // [defaults objectForKey:kVersion];
-
-
-
-    //read the NSUserDefaults settings bundle
-
-//for testing what is written, can be rem'd out later
-    NSLog(@"tester      :%@",[defaults objectForKey:kTester]);
-    NSLog(@"subject     :%@",[defaults objectForKey:kSubject]);
-    NSLog(@"email       :%@",[defaults objectForKey:kEmail]);
-    NSLog(@"start       :%@",[defaults objectForKey:kStart]);
-    NSLog(@"finish      :%@",[defaults objectForKey:kFinish]);
-    NSLog(@"size        :%@",[defaults objectForKey:kSize]);
-    NSLog(@"forward     :%@",[defaults objectForKey:kForward]);
-    NSLog(@"info        :%@",[defaults objectForKey:kInfo]);
-    NSLog(@"rotation    :%@",[defaults objectForKey:kRot]);
-    NSLog(@"block col   :%@",[defaults objectForKey:kBlockCol]);
-    NSLog(@"show col    :%@",[defaults objectForKey:kShowCol]);
-    NSLog(@"back col    :%@",[defaults objectForKey:kBackCol]);
-    NSLog(@"delay       :%@",[defaults objectForKey:kDelay]);
-    NSLog(@"time        :%@",[defaults objectForKey:kTime]);
-    NSLog(@"show        :%@",[defaults objectForKey:kShow]);
-    NSLog(@"version     :%@",[defaults objectForKey:kVersion]);
-
-    blockSizeLBL.text=[[NSString alloc]initWithFormat:@"%i",blockSize];
-    [self updateBlockColours];
-    [self updateSizesOfBlocks];
-    [self updateBlockNumbers];
-    [self updateTiming];
-
-
 }
 
 -(void)hideAllBlocks{
@@ -541,43 +481,56 @@
     if(currentBlockColour  == nil ){
         currentBlockColour =  [UIColor blueColor];
     }
+    
     currentBackgroundColour          = [defaults objectForKey:kBackCol];
     if(currentBackgroundColour  == nil ){
         currentBackgroundColour =  [UIColor blackColor];
     }
+    
     currentShowColour          = [defaults objectForKey:kShowCol];
     if(currentShowColour  == nil ){
         currentShowColour =  [UIColor blueColor];
     }
+    
     NSString *temp        = [defaults objectForKey:kStart];
     if( temp == nil ){
         start =  3;
+        [defaults setObject:@"3" forKey:kStart];
     }else{
         start=[temp intValue];
+        [defaults setObject:temp forKey:kStart];
     }
     temp        = [defaults objectForKey:kFinish];
     if( temp == nil ){
         finish =  9;
+        [defaults setObject:@"9" forKey:kFinish];
     }else{
         finish=[temp intValue];
+        [defaults setObject:temp forKey:kFinish];
     }
     temp        = [defaults objectForKey:kSize];
     if( temp == nil ){
         blockSize =  30;
+        [defaults setObject:@"30" forKey:kSize];
     }else{
         blockSize=[temp intValue];
+        [defaults setObject:temp forKey:kSize];
     }
     temp        = [defaults objectForKey:kDelay];
     if( temp == nil ){
         waitTime=  500;
+        [defaults setObject:@"500" forKey:kDelay];
     }else{
         waitTime=[temp intValue];
+        [defaults setObject:temp forKey:kDelay];
     }
     temp        = [defaults objectForKey:kShow];
     if( temp == nil ){
         showTime =  200;
+        [defaults setObject:@"200" forKey:kShow];
     }else{
         showTime=[temp intValue];
+        [defaults setObject:temp forKey:kShow];
     }
     temp        = [defaults objectForKey:kTime];
     if( temp == nil ){
@@ -614,11 +567,104 @@
     singleton.currentBlockColour=[UIColor blueColor];
     singleton.currentShowColour=[UIColor orangeColor];
     //get pos of centres
-
+    
+    //for testing what is written, can be rem'd out later
+    NSLog(@"tester      :%@",[defaults objectForKey:kTester]);
+    NSLog(@"subject     :%@",[defaults objectForKey:kSubject]);
+    NSLog(@"email       :%@",[defaults objectForKey:kEmail]);
+    NSLog(@"start       :%@",[defaults objectForKey:kStart]);
+    NSLog(@"finish      :%@",[defaults objectForKey:kFinish]);
+    NSLog(@"size        :%@",[defaults objectForKey:kSize]);
+    NSLog(@"forward     :%@",[defaults objectForKey:kForward]);
+    NSLog(@"info        :%@",[defaults objectForKey:kInfo]);
+    NSLog(@"rotation    :%@",[defaults objectForKey:kRot]);
+    NSLog(@"block col   :%@",[defaults objectForKey:kBlockCol]);
+    NSLog(@"show col    :%@",[defaults objectForKey:kShowCol]);
+    NSLog(@"back col    :%@",[defaults objectForKey:kBackCol]);
+    NSLog(@"delay       :%@",[defaults objectForKey:kDelay]);
+    NSLog(@"time        :%@",[defaults objectForKey:kTime]);
+    NSLog(@"show        :%@",[defaults objectForKey:kShow]);
+    NSLog(@"version     :%@",[defaults objectForKey:kVersion]);
     [self setRot90];
     [self updateSizesOfBlocks];
     [self updateBlockNumbers];
+}
 
+-(UIColor*)colourPicker:(NSString*)colourName{
+    UIColor *colourUIName;
+    //make an array of colour names
+    NSArray *items = @[
+                       @"Black", @"Blue", @"Green", @"Red", @"Cyan",
+                       @"White", @"Yellow", @"Magenta", @"Grey",
+                       @"Orange", @"Brown", @"Purple",
+                       @"Dark Grey", @"Light Grey"
+                       ];
+    //find the index value of each
+    long item = [items indexOfObject: colourName];
+    
+    //select the item number
+    switch (item) {
+        case 0:
+            // Item 1
+            colourUIName = [UIColor blackColor];
+            break;
+        case 1:
+            // Item 2
+            colourUIName = [UIColor blueColor];
+            break;
+        case 2:
+            // Item 3
+            colourUIName = [UIColor greenColor];
+            break;
+        case 3:
+            // Item 4
+            colourUIName = [UIColor redColor];
+            break;
+        case 4:
+            // Item 5
+            colourUIName = [UIColor cyanColor];
+            break;
+        case 5:
+            // Item 6
+            colourUIName = [UIColor whiteColor];
+            break;
+        case 6:
+            // Item 7
+            colourUIName = [UIColor yellowColor];
+            break;
+        case 7:
+            // Item 8
+            colourUIName = [UIColor magentaColor];
+            break;
+        case 8:
+            // Item 9
+            colourUIName = [UIColor grayColor];
+            break;
+        case 9:
+            // Item 10
+            colourUIName = [UIColor orangeColor];
+            break;
+        case 10:
+            // Item 11
+            colourUIName = [UIColor brownColor];
+            break;
+        case 11:
+            // Item 12
+            colourUIName = [UIColor purpleColor];
+            break;
+        case 12:
+            // Item 13
+            colourUIName = [UIColor darkGrayColor];
+            break;
+        case 13:
+            // Item 14
+            colourUIName = [UIColor lightGrayColor];
+            break;
+        default:
+            colourUIName = [UIColor orangeColor];
+            break;
+    }
+    return colourUIName;
 }
 
 #pragma mark Settings Actions Buttons
